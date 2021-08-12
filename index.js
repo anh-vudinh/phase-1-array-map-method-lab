457200005545810586363f9d8dc1b4d454c9065f
Anh-vu <anhuvdinh@gmail.com>
2021-08-12 15:57:41 -0500

    Done.

--- index.js
+++ index.js
@@ -9,8 +9,33 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-];
+]
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased(param){
+  //const newArray = tutorials.map(sentence => sentence.charAt(0).toUpperCase()+sentence.slice(1));
+  const newArray = tutorials.map(sentence => {
+    const spaces = sentence.split(" ");
+    const newArray2 = spaces.map(word => word.charAt(0).toUpperCase()+word.slice(1));
+    const newSentence = newArray2.join(" ");
+    return newSentence;
+    //return sentence.charAt(0).toUpperCase()+sentence.slice(1)
+  });
+  console.log(newArray);
+  return newArray;
+;
 }
+
+console.log(titleCased(tutorials));
+
+// function titleCased{
+//   const tutorial = tutorials.map(function(string){string.charAt(0).toUpperCase()+string.slice(1);});
+// }
+
+// function titleCased(x){
+//   let w = x.charAt(0).toUpperCase()+x.slice(1);
+//   return w;
+// }
+
+
+// console.log(test(y));
